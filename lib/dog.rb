356d53ee7984dc4ce8adb9e2103e7ddef0a03225
Zackary Cammack <zack.cammack@gmail.com>
2020-08-28 14:58:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def print_all
+    @@all.each do |dog|
+      puts "#{@name}"
+    end
+  end
+
+  
+  
+end
