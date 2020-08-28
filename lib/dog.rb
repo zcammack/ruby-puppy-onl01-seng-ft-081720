67898ee1c0d78386f0de11a1e5533847b6b96b81
Zackary Cammack <zack.cammack@gmail.com>
2020-08-28 20:18:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
@@ -17,8 +17,12 @@ class Dog
 
   def self.print_all
     @@all.each do |dog|
-      puts dog.name
+      puts dog.@name
     end
+  end
+
+  def save
+    @@all << self
   end
 
 end
