654ffd6de4ecdf813752df0f9927ab73de626738
Zackary Cammack <zack.cammack@gmail.com>
2020-08-28 20:45:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require "pry"
+
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    @@all.map do |dog|
+      puts dog.name
+    end
+  end
+
+  def save
+    @@all << self
+  end
+
+end
